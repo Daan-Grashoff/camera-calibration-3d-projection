208ea279544970b9896a8e3fcc718f4b51b66765
Daan Grashoff <daan_donn@hotmail.com>
2020-02-06 12:27:44 +0100

    Initial Commit

---
+++ Computer vision/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Computer vision
+//
+//  Created by Daan Grashoff on 06/02/2020.
+//  Copyright © 2020 Daan Grashoff. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
